e7aca45f809ec0d7928db36e6909267c73e56d0e
Justin Hamilton <justinanthonyhamilton@gmail.com>
2018-01-27 11:32:04 -0800

    Adding basic tiled maps and sets.

---
+++ assets/tiled/sets/sneky_tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Sneky_Tiles" tilewidth="182" tileheight="106" tilecount="2" columns="0">
+ <grid orientation="isometric" width="1" height="1"/>
+ <tile id="0">
+  <image width="182" height="106" source="../../textures/tile_dirt.png"/>
+ </tile>
+ <tile id="1">
+  <image width="182" height="106" source="../../textures/tile_grass.png"/>
+ </tile>
+</tileset>
