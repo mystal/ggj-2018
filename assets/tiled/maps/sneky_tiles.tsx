e2c787229f94d3853b31aa803d944fb328710092
Justin Hamilton <justinanthonyhamilton@gmail.com>
2018-01-27 16:30:04 -0800

    Adding more levels, embedding tilesets.

--- assets/tiled/maps/sneky_tiles.tsx
+++ assets/tiled/maps/sneky_tiles.tsx
@@ -1,22 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="sneky_tiles" tilewidth="182" tileheight="121" tilecount="6" columns="0">
+<tileset name="sneky_tiles" tilewidth="182" tileheight="106" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="182" height="106" source="../../textures/tile_grass.png"/>
  </tile>
  <tile id="1">
   <image width="182" height="106" source="../../textures/tile_dirt.png"/>
- </tile>
- <tile id="2">
-  <image width="89" height="105" source="../../textures/mailbox.png"/>
- </tile>
- <tile id="3">
-  <image width="124" height="121" source="../../textures/pug.png"/>
- </tile>
- <tile id="4">
-  <image width="163" height="88" source="../../textures/sneky_fox.png"/>
- </tile>
- <tile id="5">
-  <image width="61" height="57" source="../../textures/letter_1.png"/>
  </tile>
 </tileset>
