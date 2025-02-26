242eb0a175f4d865cf433bf47550ce1c7bb04bb5
Gabriel Martinez <reitaka@gmail.com>
2025-02-26 11:49:25 -0800

    Stub in rest of levels and make credits screen
    Also scale down the fox to match the original game.

--- godot/ui/menus/menu_screen.gd
+++ godot/ui/menus/menu_screen.gd
@@ -1,9 +1,11 @@
-class_name TitleScreen
+class_name MenuScreen
 extends Control
+
+@export var NEXT_LEVEL: PackedScene
 
 func _ready() -> void:
 	AudioManager.play_bgm()
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://levels/mockup_level.tscn")
+		get_tree().change_scene_to_packed(NEXT_LEVEL)
