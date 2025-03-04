3fa9bbcd04947afef3e9fbe7523badc1c02f9c10
Gabriel Martinez <reitaka@gmail.com>
2025-03-03 20:09:58 -0800

    Fix circular reference bug

--- godot/ui/menus/menu_screen.gd
+++ godot/ui/menus/menu_screen.gd
@@ -1,11 +1,9 @@
 class_name MenuScreen
 extends Control
 
-@export var NEXT_LEVEL: PackedScene
-
 func _ready() -> void:
 	AudioManager.play_bgm()
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
-		get_tree().change_scene_to_packed(NEXT_LEVEL)
+		get_tree().change_scene_to_file("res://levels/mockup_level.tscn")
